a30a32ac76d7df1b99aeb49c739b188a329496a8
Adilson de Lima nascimento <60784414+DurdenTy@users.noreply.github.com>
2021-06-18 17:16:31 -0300

    Create main.java

---
+++ Strategy/Fretes/main.java
@@ -0,0 +1,13 @@
+package Frete;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+		ContratanteDoServico cs = new ContratanteDoServico (new FreteExpresso(), 42);
+		
+
+	}
+
+}
