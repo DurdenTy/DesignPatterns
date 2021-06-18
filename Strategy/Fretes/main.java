f92ddb9a8da4b5397e014fac894aa0203a6557de
Adilson de Lima nascimento <60784414+DurdenTy@users.noreply.github.com>
2021-06-18 17:17:47 -0300

    Update main.java

--- Strategy/Fretes/main.java
+++ Strategy/Fretes/main.java
@@ -6,7 +6,8 @@ public class main {
 		
 		
 		ContratanteDoServico cs = new ContratanteDoServico (new FreteExpresso(), 42);
-		
+		ContratanteDoServico cs2 = new ContratanteDoServico (new FreteNormal(), 42);
+		ContratanteDoServico cs3 = new ContratanteDoServico (new FreteGratis(), 42);
 
 	}
 
