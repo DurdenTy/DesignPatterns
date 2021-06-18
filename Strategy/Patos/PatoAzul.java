bc3dc09fc747b9eaa87e68b7a298c54abc898816
Adilson de Lima nascimento <60784414+DurdenTy@users.noreply.github.com>
2021-06-18 16:39:37 -0300

    Add files via upload

---
+++ Strategy/Patos/PatoAzul.java
@@ -0,0 +1,13 @@
+import strategies.SomPato;
+import strategies.VoarPato;
+
+public class PatoAzul extends Pato {
+
+	public PatoAzul() {
+		
+		cvoar = new VoarPato();
+		csom = new SomPato();
+		
+	}
+	
+}
