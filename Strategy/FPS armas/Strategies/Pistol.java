8c3e09019b36f9b46a473c44b3e0f5b619b255a8
Adilson de Lima nascimento <60784414+DurdenTy@users.noreply.github.com>
2021-06-21 17:29:27 -0300

    Add files via upload

---
+++ Strategy/FPS armas/Strategies/Pistol.java
@@ -0,0 +1,12 @@
+package strategies;
+
+public class Pistol implements Dano {
+	
+	
+	private double dano = 30;
+	
+	public double infligirDano() {
+		return dano;
+	}
+	
+}
---
+++ Strategy/FPS armas/Strategies/Shotgun.java
@@ -0,0 +1,11 @@
+package strategies;
+
+public class Shotgun implements Dano {
+
+	private double dano = 70;
+	
+	public double infligirDano() {
+		return dano;
+	}
+	
+}
