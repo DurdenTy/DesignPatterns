86179af1019c08ecc6210fdbcecfddd769332757
Adilson de Lima nascimento <60784414+DurdenTy@users.noreply.github.com>
2021-06-24 16:07:15 -0300

    Create main.java

---
+++ Observer/main.java
@@ -0,0 +1,15 @@
+
+
+
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		WeatherData wd = new WeatherData();
+		Tela t = new Tela(wd);
+		wd.setModificacoes(31, 80, 0);
+		
+	}
+
+}
