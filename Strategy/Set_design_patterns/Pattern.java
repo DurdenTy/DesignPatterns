b6bdac415bc16cebbd310ba29456c7d9edf2e305
Adilson de Lima nascimento <60784414+DurdenTy@users.noreply.github.com>
2021-06-24 12:32:24 -0300

    Add files via upload

---
+++ Strategy/Set_design_patterns/Pattern.java
@@ -0,0 +1,13 @@
+package Frete;
+
+import strategies.Strategy;
+
+public class Pattern extends Programmer {
+
+	public Pattern() {
+		
+		dp = new Strategy();
+		
+	}
+	
+}
---
+++ Strategy/Set_design_patterns/Programmer.java
@@ -0,0 +1,19 @@
+package Frete;
+
+import strategies.DesignPatterns;
+
+public class Programmer {
+
+	DesignPatterns dp;
+	
+	public void getPattern() {
+		
+		dp.ActualMethod();
+		
+	}
+	
+	public void setPattern(DesignPatterns sdp) {
+		dp = sdp;
+	}
+	
+}
